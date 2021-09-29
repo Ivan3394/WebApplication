bbd3c3689b2ec4a0760462dc9bbb2d5019a2c686
IvanRus33 <91609598+IvanRus33@users.noreply.github.com>
2021-09-29 17:03:57 +0300

    Update IndexServlet.java

--- IndexServlet.java
+++ IndexServlet.java
@@ -8,20 +8,17 @@ package Servlets;
 import DB.ItProduct;
 import DB.ItProductDB1;
 import java.io.IOException;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.annotation.Resource;
-import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.sql.DataSource;
 
 
 @WebServlet("/")
