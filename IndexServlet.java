fafea6029f0b5dfff80cbfcb9e1732d0e31da25b
IvanRus33 <91609598+IvanRus33@users.noreply.github.com>
2021-09-29 17:01:07 +0300

    Update IndexServlet.java

--- IndexServlet.java
+++ IndexServlet.java
@@ -8,12 +8,8 @@ package Servlets;
 import DB.ItProduct;
 import DB.ItProductDB1;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -30,52 +26,11 @@ import javax.sql.DataSource;
 
 @WebServlet("/")
 public class IndexServlet extends HttpServlet {
-
-    private static String url = "jdbc:mysql://localhost:3306/ItProductDB?serverTimezone=Europe/Moscow&useSSL=false"; //jdbc:mysql://localhost/groupsdb?serverTimezone=Europe/Moscow&useSSL=false
-    private static String username = "Ivan";
-    private static String password = "12345678";
-
+    
     @Resource(name = "jdbc/itproduct")
     private DataSource ds;
     
-    @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        
-//        resp.setContentType("text/html");
-//        PrintWriter out = resp.getWriter();
-//        Connection con = null;
-//        Statement stmt = null;
-//        ResultSet rs = null;
-//        
-//        try {
-//            Class.forName("com.mysql.jdbc.Driver");
-//            InitialContext ctx;
-//            ctx = new InitialContext();
-//            DataSource ds = (DataSource) ctx.lookup("java:comp/env/jdbc/itproduct");
-//            Connection conn = ds.getConnection();
-//            
-////            con = DriverManager.getConnection (url, username, password);
-//            stmt = con.createStatement();
-//            rs = stmt.executeQuery("select * from itproduct");
-//            while(rs.next()) {
-//                    out.println(rs.getInt("id"));
-//                    out.println(rs.getString("name"));
-//                    out.println(rs.getInt("price"));
-//                out.println("<br>");
-//            }
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        } catch (NamingException ex) {
-//            Logger.getLogger(IndexServlet.class.getName()).log(Level.SEVERE, null, ex);
-//        } catch (ClassNotFoundException ex) {
-//            Logger.getLogger(IndexServlet.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//            throw new ServletException(/*"Servlet Could not display records.",*/ e);
-//        }catch (ClassNotFoundException e) {
-//            throw new ServletException(/*"JDBC Driver not found.",*/ e);
-//        } catch (NamingException ex) {
-//            Logger.getLogger(IndexServlet.class.getName()).log(Level.SEVERE, null, ex);
-//        }
         
         
             ArrayList<ItProduct> itproducts = ItProductDB1.select();
